ef499e0b510e6990d63408597e35afad9e69ac11
reza <mardanireza58@gmail.com>
2024-05-06 17:48:37 +0330

    added: response mosel - status code - httpExcetion

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from fastapi import FastAPI, Query,Path
+from fastapi import FastAPI, Query,Path, status, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
 
-class Invalidation(BaseModel):
-   name : str | None = Query(max_length=5)
-   age : int | None
+class UserIn(BaseModel):
+  name:  str | None = Query(max_length=10)
+  email: str | None = Query(max_length=20)
+  password: str | None = Query(min_length=8)
 
-@app.post('/register/{ids}')
-async def getName(obj:Invalidation, ids: int = Path(lt=50)):
-   return obj, ids
+
+class UserOut(BaseModel):
+   name: str
+   email: str
+
+
+@app.post('/register', response_model=UserOut, status_code=status.HTTP_200_OK)
+async def getName(user: UserIn):
+   if user.name == "admin":
+      raise HTTPException(status.HTTP_400_BAD_REQUEST, "user cant set name: admin")
+   return user
