b9e0970e67d6b8fe40de3e0ca55d03dbf3d2a29a
reza <mardanireza58@gmail.com>
2024-05-07 21:26:56 +0330

    fixed: bug in create user view

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def get_db():
 
 @app.post("/users", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = db.query(models.User).filter(models.User.email==user.email).fitst()
+    db_user = db.query(models.User).filter(models.User.email==user.email).first()
     if db_user:
         raise HTTPException(status_code=400 , detail="user are exist!")
     user = models.User(email=user.email, username=user.username, password=user.password)
@@ -26,3 +26,9 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(user)
     return user
+
+
+@app.get('/users/{user_id}', response_model=schemas.User)
+async def get_user(user_id:int, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    return db_user
